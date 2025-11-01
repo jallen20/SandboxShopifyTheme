e5c5a12e8a34a92254fb577cac5d6520aebf2e34
Justin Allen <allen.justin78@gmail.com>
2025-11-01 14:40:15 -0400

    Added mobile first styling

--- assets/p-nav.js
+++ assets/p-nav.js
@@ -1,18 +1,16 @@
-(function() {
-    'use strict';
-    let selectedNav = undefined;
-    const initNav = (self, event, container) => {
-        if (selectedNav) {
-            selectedNav.classList.remove('active');
-        }
-        selectedNav = event.target;
+export const initNav = (event, onNavEvent) => {
+    console.log("INIT NAV")
+    let activeNav = document.querySelector('.active')
+    console.log("activeNav", activeNav)
+    if (activeNav && [activeNav]?.length) {
+        [activeNav].forEach(n => n.classList.remove('active'));
+        const selectedNav = event.target;
         selectedNav.classList.add('active');
-
+        onNavEvent(selectedNav);
+    } else {
+        activeNav = document.getElementById("nav_home");
+        activeNav.classList.add('active');
     }
 
-    const nav = document.getElementById('p-nav');
-    console.log('Nav', nav);
-    nav.addEventListener('click', (e) => {
-        console.log("Clicked ->", e);
-    })
-})();
+
+}
